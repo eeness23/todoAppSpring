ec4502189a6a568fc5115da1c9f86a665a2b8965
Enes Demirbaş <enesdemirbas95@gmail.com>
2019-07-23 12:35:20 +0300

    first commit

---
+++ src/main/java/com/enes/fullstacktodoapp/full_stack_todo_app/FullStackTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.enes.fullstacktodoapp.full_stack_todo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackTodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackTodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enes/fullstacktodoapp/full_stack_todo_app/FullStackTodoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.enes.fullstacktodoapp.full_stack_todo_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FullStackTodoAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
