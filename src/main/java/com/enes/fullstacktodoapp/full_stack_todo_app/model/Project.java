6fb86c372c368465da2a1aec26c9577e72cd7760
Enes Demirbaş <enesdemirbas95@gmail.com>
2019-07-23 13:00:15 +0300

    created project model and validation

---
+++ src/main/java/com/enes/fullstacktodoapp/full_stack_todo_app/model/Project.java
@@ -0,0 +1,110 @@
+package com.enes.fullstacktodoapp.full_stack_todo_app.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Project name is requried")
+    private String projectName;
+    @NotBlank(message = "Project Identifier name is requried")
+    @Size(min = 3,max = 5, message = "Please use 3 to 5 characters")
+    @Column(updatable = false,unique = true)
+    private String projectIdentifier;
+    @NotBlank(message = "Description is requried")
+    private String desc;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date end_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date create_at;
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date updated_at;
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public Date getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(Date start_date) {
+        this.start_date = start_date;
+    }
+
+    public Date getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(Date end_date) {
+        this.end_date = end_date;
+    }
+
+    public Date getCreate_at() {
+        return create_at;
+    }
+
+    public void setCreate_at(Date create_at) {
+        this.create_at = create_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.create_at=new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_at=new Date();
+    }
+
+}
